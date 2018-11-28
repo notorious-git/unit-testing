2dd2926bc3d6ad930eb09bb6d684f55c9c257edd
Peter Jang <peter.x.jang@gmail.com>
2018-11-27 18:15:43 -0600

    Initial commit

---
+++ calculator.rb
@@ -0,0 +1,33 @@
+class Calculator
+  def add(number_one, number_two)
+    return number_one + number_two
+  end
+
+  def subtract(number_one, number_two)
+    return number_one - number_two
+  end
+
+  def multiply(number_one, number_two)
+    return number_one * number_two
+  end
+
+  def divide(dividend, divisor)
+    return dividend / divisor
+  end
+
+  def sqaure(number)
+    return square * square
+  end
+
+  def power(number, exponent)
+    return number ** exponent
+  end
+end
+
+calculator = Calculator.new
+puts "Testing the add method"
+if calculator.add(1, 1) == 2
+  puts "Test passed!"
+else
+  puts "Test failed!"
+end
