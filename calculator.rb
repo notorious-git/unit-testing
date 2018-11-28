c225a64343a79beee439377b42d3a1e497e95f1e
Peter Jang <peter.x.jang@gmail.com>
2018-11-27 19:06:38 -0600

    Add remaining tests

--- calculator.rb
+++ calculator.rb
@@ -17,12 +17,16 @@ class Calculator
     return dividend / divisor
   end
 
-  def sqaure(number)
-    return square * square
+  def square(number)
+    return number * number
   end
 
   def power(number, exponent)
     return number ** exponent
+  end
+
+  def add_three(number)
+    return number + 3
   end
 end
 
@@ -38,6 +42,54 @@ RSpec.describe Calculator do
       calculator = Calculator.new
       result = calculator.add(1, -3)
       expect(result).to eq(-2)
+    end
+  end
+
+  describe "#subtract" do
+    it "should return the difference of two numbers" do
+      calculator = Calculator.new
+      result = calculator.subtract(5, 3)
+      expect(result).to eq(2)
+    end
+  end
+
+  describe "#multiply" do
+    it "should return the product of two numbers" do
+      calculator = Calculator.new
+      result = calculator.multiply(4, 2)
+      expect(result).to eq(8)
+    end
+  end
+
+  describe "#divide" do
+    it "should return the quotient of two numbers" do
+      calculator = Calculator.new
+      result = calculator.divide(40, 2)
+      expect(result).to eq(20)
+    end
+  end
+
+  describe "#square" do
+    it "should return a number times itself" do
+      calculator = Calculator.new
+      result = calculator.square(5)
+      expect(result).to eq(25)
+    end
+  end
+
+  describe "#power" do
+    it "should return a number times itself by the given exponent" do
+      calculator = Calculator.new
+      result = calculator.power(2, 4)
+      expect(result).to eq(16)
+    end
+  end
+
+  describe "#add_three" do
+    it "should add 3 to a given number" do
+      calculator = Calculator.new
+      result = calculator.add_three(7)
+      expect(result).to eq(10)
     end
   end
 end
